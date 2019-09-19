376066a02ab264aae2d72954f7444931ec7c53ca
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-09-19 04:35:10 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,47 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+puts sum 
 end
+
+# The Addition Argument 
+addition (2, 4)
 
 def subtraction(num1, num2)
-
+difference = num1 - num2
+puts difference 
 end
+
+# The Difference Argument
+subtraction (2, 4)
 
 def division(num1, num2)
-
+divide = num1 / num2
+puts divide
 end
+
+# The Divivision Argument
+division (4, 2)
 
 def multiplication(num1, num2)
-
+multiply = num1 * num2
+puts multiply
 end
+
+# The Multiplication Argument
+multiplication (4, 2)
 
 def modulo(num1, num2)
-
+remainder = num1 % num2
+puts remainder
 end
+
+# The Remainder Argument
+modulo (5, 2)
 
 def square_root(num)
-
+squareroot = sqrt(num)
+puts squareroot 
 end
+
+# The Squareroot Argument
+square_root (100)
